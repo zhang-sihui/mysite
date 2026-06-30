e5b9bf0ca67d55da54b3e1de48c45805269c7714
zhangsh <zhang.sihui@qq.com>
2026-06-30 13:30:37 +0800

    fix: 修复一些问题 1.减小评论输入框的 padding; 2.登录时 email 为空设置默认值。

--- index/users.py
+++ index/users.py
@@ -24,7 +24,7 @@ def login(request, pathname):
                     request.session['logged'] = True
                     request.session['id'] = user_data.id
                     request.session['username'] = user_data.username
-                    request.session['email'] = user_data.email
+                    request.session['email'] = user_data.email or ''
                     return redirect(f'{pathname}')
                 else:
                     request.session['incorrect_password'] = True
